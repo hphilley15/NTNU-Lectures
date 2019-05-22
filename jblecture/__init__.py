6358a677b5e48f4fae731b52768e7d4e0343dfa9
cvroberto21 <47555203+cvroberto21@users.noreply.github.com>
2019-05-22 20:20:26 +0800

    Update __init__.py

--- jblecture/__init__.py
+++ jblecture/__init__.py
@@ -222,6 +222,7 @@ def createEnvironment( params = {} ):
 
     with jbcd.JBcd( cfg['ROOT_DIR'] / 'reveal.js' ):
         print("Executing npm install")
+	o = None
         try:
             o = subprocess.check_output("npm install", shell = True)
         except subprocess.CalledProcessError:
@@ -231,6 +232,7 @@ def createEnvironment( params = {} ):
 
     with jbcd.JBcd( cfg['ROOT_DIR'] / 'reveal.js' ):
         print("Executing npm install decktape")
+	o = None
         try:
             o = subprocess.check_output("npm install decktape", shell = True)
         except subprocess.CalledProcessError:
