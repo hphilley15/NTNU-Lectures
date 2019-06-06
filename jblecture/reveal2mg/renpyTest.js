3f29ea9bc080b690ca8ae95079956eade8d3d6f8
negrin006 <negrin006@outlook.com>
2019-06-06 14:18:57 +0800

    grammar

--- jblecture/reveal2mg/renpyTest.js
+++ jblecture/reveal2mg/renpyTest.js
@@ -1,32 +1,39 @@
 const parser = require("./renpy.js")
 
 tests = [   "'xxyyx cvcvc xxx'", 
-            '"This is the new lecture style"', 
+            '  \n"This is the new lecture style"  \n', 
             '"""A\nmultiline\nstring"""', 
             "'''Another\r\ntest'''" ]
 for(var i = 0; i < tests.length; i++ ) {
     try{
+        console.log("Trying to parse |" + tests[i] + "|");
         parser.parse(tests[i]);
         console.log('Success');
     }
     catch(err) {
         console.log('Syntax Error');
-        console.log(err)
+        console.log(err);
+        throw( err );
     }
 } 
 
 tests = [ 
 "label test:\n" +
 "       \"My message is clear!\"",
+"label test2:\n" +
+"   \"bob\" \"Hello\"\n" +
+"  \t \"marry\" \"Hello Bob\"\n"
 ]
 
 for(var i = 0; i < tests.length; i++ ) {
     try{
+        console.log("Trying to parse |" + tests[i] + "|");
         parser.parse(tests[i]);
         console.log('Success');
     }
     catch(err) {
         console.log('Syntax Error');
-        console.log(err)
+        console.log(err);
+        throw( err );
     }
 } 
