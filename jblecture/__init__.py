4aaf06e95df2d798a97b8b8966428d3e62ecd5c5
Marcella Schutz <athil023@protonmail.com>
2019-07-11 11:30:48 +0800

    Theme fixed

--- jblecture/__init__.py
+++ jblecture/__init__.py
@@ -65,7 +65,7 @@ defaults['REVEAL_PRESENTATION_TEMPLATE'] = """
         <title>{{title}}</title>
 
         <link rel="stylesheet" href="css/reveal.css">
-        <link rel="stylesheet" href="css/theme/{{ REVEAL_THEME }}.css">
+        <link rel="stylesheet" href="css/theme/{{ theme }}.css">
 
         <!-- Theme used for syntax highlighting of code -->
         <link rel="stylesheet" href="lib/css/zenburn.css">
