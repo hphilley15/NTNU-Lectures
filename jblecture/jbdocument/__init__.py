400233c6cea0efc021707a5983e2fa2a2fe25b60
Jacky Baltes <jacky@cs.umanitoba.ca>
2019-08-28 17:32:43 +0800

    test

--- jblecture/jbdocument/__init__.py
+++ jblecture/jbdocument/__init__.py
@@ -151,7 +151,7 @@ class JBDocument:
     def createAssets( self, assets ):
         s = "assets = {\n"
         for a in assets:
-            print(assets[a])
+            print(assets[a], assets[a].name)
             s = s + "    " + "{" + assets[a].name + "," + assets[a].url + "," + assets[a].localFile + "}\n"
         s = s + "}\n"
     
