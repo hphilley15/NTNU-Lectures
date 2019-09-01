2efe8b5954729bfb4d2f691118d57ae7965af8c4
negrin006 <negrin006@outlook.com>
2019-09-01 14:26:12 +0800

    Commit

--- html/ntnu.js
+++ html/ntnu.js
@@ -1,3 +1,24 @@
+function updateAsset( id, asset, mode ) {
+    newContent = "";
+    if ( mode == "local" ) {
+        newContent = "<img id=\"img-" + id + "\" src=\"" + asset[2] + "\"/>";
+    } else if ( mode == "url" ) {
+        newContent = "<img id=\"img-" + id + "\" src=\"" + asset[1] + "\"/>";
+    }
+    console.log("updateAsset(" + id + "," + asset + "," + mode + ") =>" + newContent );
+    return newContent;
+}
+
 function convertURLs( assets, mode ) {
     console.log("convertURLs " + mode );
+    for( id in assets ) {
+        console.log("Upadting id " + id);
+        el = document.getElementById( id );
+        if ( el != null ) {
+            console.log("el " + el );
+            asset = assets[ id ];
+            newContent = updateAsset( id, asset, mode );
+            el.innerHTML = newContent;
+        }
+    }
 }
