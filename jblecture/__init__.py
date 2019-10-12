30fc051e1e9b5c4399e3e1abe91827eab1709f83
Marcella Schutz <athil023@protonmail.com>
2019-10-12 18:32:12 +0800

    math

--- jblecture/__init__.py
+++ jblecture/__init__.py
@@ -59,8 +59,8 @@ except ImportError:
 # Reveal.js Parameters
 defaults['REVEAL_THEME'] = 'ntnuerc'
 
-print('*** CWD', os.getcwd() )
-with open("./NTNU-Lectures/html/index_template.html") as f:
+print('*** CWD', os.getcwd(), str(defaults['MODULE_ROOT']) )
+with open( defaults['MODULE_ROOT'] / 'html' / 'index_template.html') as f:
     defaults['REVEAL_PRESENTATION_TEMPLATE'] = f.read()
 
 defaults['REVEAL_SLIDE_TEMPLATE'] = """
