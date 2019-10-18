85eb8705dd38332aeeed8b28dfb4dcb7259a33b8
dkclauson003 <dkclauson003@outlook.com>
2019-10-18 01:06:53 -0400

    port

--- jblecture/__init__.py
+++ jblecture/__init__.py
@@ -349,7 +349,7 @@ def startLocalServer( ):
             cfg['HTTPD'] = httpd
             cfg['HTTP_PORT'] = port
 
-        httpd.serve_forever()
+            httpd.serve_forever()
 
     print("Starting server thread")
     thread = threading.Thread( target=server_entry )
