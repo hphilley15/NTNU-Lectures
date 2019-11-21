b9383b05017b2ac0be42fdabdd26f1d673f4f05a
dkclauson003 <dkclauson003@outlook.com>
2019-11-21 22:18:22 +0100

    GitHub

--- jblecture/jbgithub/__init__.py
+++ jblecture/jbgithub/__init__.py
@@ -64,7 +64,7 @@ def runCommand( cmd ):
     try:
         o = subprocess.check_output( cmd, shell=True)
     except subprocess.CalledProcessError as error:
-        print("Command returned error CalledProcessError", error, error.output)
+        print("Command returned error CalledProcessError", error, error.stderr)
     if ( o ):
         print( "Output " + cmd + ":\n" + o.decode('utf-8') )
 
