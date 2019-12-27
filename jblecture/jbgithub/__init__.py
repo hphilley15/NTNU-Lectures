ecc836c9f0840958182dfae5d9ab2f1808096609
Yuri Poplr <yuripoplr007@outlook.com>
2019-12-27 05:27:18 -0600

    github

--- jblecture/jbgithub/__init__.py
+++ jblecture/jbgithub/__init__.py
@@ -150,8 +150,9 @@ def createGitHub( title, root = None):
 
     with JBcd(p):
         runCommand( cfg['GIT_CMD'] + " push" )
-
-        #runCommand( cfg['GIT_CMD'] + " push origin gh-pages" )
+        runCommand( cfg['GIT_CMD'] + " push --set-upstream origin gh-pages" )
+    
+        runCommand( cfg['GIT_CMD'] + " push origin gh-pages" )
     
             # if not p.is_dir():
             #     print("cloning {0} from url {1} root {2}".format( dirname, url, root ), 'git command', cfg['GIT_CMD'])
